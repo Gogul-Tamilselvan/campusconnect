2552a9dc34f66d3c8c6fda254d5ffdf281a0407d
Gogul <gokulglorious07@gmail.com>
2025-10-28 04:35:16 +0000

    replace exisiting firebase config with this // Import the functions you

--- src/firebase/provider.tsx
+++ src/firebase/provider.tsx
@@ -5,12 +5,12 @@ import { getFirestore, Firestore } from 'firebase/firestore';
 import { getAuth, Auth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB8ahNcHBe4tTBi1mrJINcXcl_QQXt3OYo",
-  authDomain: "campusconnectz.firebaseapp.com",
-  projectId: "campusconnectz",
-  storageBucket: "campusconnectz.appspot.com",
-  messagingSenderId: "620187555067",
-  appId: "1:620187555067:web:60dadc881f97b1a31e4c5e"
+  apiKey: "AIzaSyBXacMrzHsD7kCI6RNoJELsHIcZKwFAhOA",
+  authDomain: "campusconnect-1318a.firebaseapp.com",
+  projectId: "campusconnect-1318a",
+  storageBucket: "campusconnect-1318a.appspot.com",
+  messagingSenderId: "1001319904361",
+  appId: "1:1001319904361:web:43295a60b18c9d35205ef8"
 };
 
 
